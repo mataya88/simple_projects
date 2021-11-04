aa7540abc5a2ebdd3b4bc9d25990fb21927ed734
Muataz Attaia <moatazataya346@gmail.com>
2021-11-05 01:01:12 +0300

    Uploaded Palindrome time Project

---
+++ Palindrome_time_checker.py
@@ -0,0 +1,13 @@
+"""
+Alongside determining whather an input is palindrome or not, the program calcultes the time taken for the process to be completed
+"""
+from time import perf_counter
+input_string = input(
+    'Please enter anything,the program will check wheather it\'s a palindrome or not.\n')
+start = perf_counter()
+if input_string == input_string[::-1]:
+    print("This is a palindrome.")
+else:
+    print('This is not a palindrome.')
+end = perf_counter()
+print(f"Taken time = {end-start}")
